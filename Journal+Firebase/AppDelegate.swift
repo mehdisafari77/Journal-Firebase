a3405d80f44658961c84885fe9f5552a3c3f1a3d
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-24 08:44:54 -0700

    testing buttons for fetching one and all

--- Journal+Firebase/AppDelegate.swift
+++ Journal+Firebase/AppDelegate.swift
@@ -13,7 +13,7 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        FirebaseApp.configure()
         return true
     }
 
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        FirebaseApp.configure()
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
